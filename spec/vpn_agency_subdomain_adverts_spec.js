6ef1037fd12d1dda395e766e41fe793cda037af9
kdl <testqa.kdl@gmail.com>
2015-07-07 11:57:56 +0300

    soma fixes

--- spec/vpn_agency_subdomain_adverts_spec.js
+++ spec/vpn_agency_subdomain_adverts_spec.js
@@ -2,8 +2,8 @@ require('./boot');
 frisby.create('Get Agency Subdomain Adverts List (sort)')
     //.addHeader('token', agencyToken)
     .get(URL_agency_subdomain_adverts+
-    '?operationType='+operationtypesId)
-    //'&category='+category_type0CategoryId)//operationtypesid=Продажа/Аренда= operationTypesRent, category_type(0..3)Id, priceFrom, priceTo, currency
+    '?operationType='+operationtypesId+
+    '&category='+category_type0CategoryId)//operationtypesid=Продажа/Аренда= operationTypesRent, category_type(0..3)Id, priceFrom, priceTo, currency
     .inspectJSON()
     .expectHeader('Content-Type', 'application/json')
     .expectStatus(200)
