db04abf3821f700f0f1585f5d2aa7cf027747525
Serhii <serhiimelnik@gmail.com>
2015-09-16 18:20:24 +0300

    fix registration pperson

--- spec/vpn_registration_pperson_spec.js
+++ spec/vpn_registration_pperson_spec.js
@@ -2,21 +2,15 @@ require('./boot');
 frisby.create('Registration. Private person')
     .post(URL_registration_pperson,{
         firstName: userFirstName,
-        lastName: userLastName,
-        email: userEmail,
-        plainPassword: userPassword,
-        phones: userPhones,
-        userAvatar: userAvatar
+        email: userEmail
     }, {json: true})
     .inspectJSON()
     .expectHeader('Content-Type', 'application/json')
     .expectJSON({
         id: String,
         firstName: String,
-        lastName: String,
         email: String,
         token: String,
-        phones: Array,
         userType: 'private person'
     })
     .afterJSON(function (json) {
